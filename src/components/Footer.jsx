3130c41f889bd363752c42b53a74785d17530cec
Yaswanth Velagapudi <58129468+yaswanth135@users.noreply.github.com>
2023-05-14 08:13:44 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import AppContext from "../provider/AppContext";
+
+function Footer() {
+  const {
+    app: { isDark },
+  } = useContext(AppContext);
+  const anchorStyle = {
+    color: "#C6C6C6",
+  };
+  return (
+    <>
+      <footer className="footer">
+        <a
+          href="https://www.instagram.com/p/CfoixJ0vM_P/"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-brands fa-instagram"></i> <span>Instagram</span>
+        </a>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
